570e2caf2a0720a7700b64d9deb042bdfdb9e0f0
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-02-21 11:37:52 +0100

    Finished (black keys added)

--- Virtual-Piano/keyboard.js
+++ Virtual-Piano/keyboard.js
@@ -27,6 +27,26 @@ document.addEventListener("keypress", function(event) {
         let audio = new Audio("white_keys/J.mp3");
         audio.play();
     }
+    else if (event.code == "KeyW") {
+        let audio = new Audio("black_keys/W.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyE") {
+        let audio = new Audio("black_keys/E.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyT") {
+        let audio = new Audio("black_keys/T.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyY") {
+        let audio = new Audio("black_keys/Y.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyU") {
+        let audio = new Audio("black_keys/U.mp3");
+        audio.play();
+    }
     else {
         console.log("Warning: unboundkey is pressed.")
     }
