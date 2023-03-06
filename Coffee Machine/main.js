7a7b2f6603c33869853e23a419b2111ad26860f4
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-06 11:55:16 +0100

    Stage 3 finished

--- Coffee Machine/main.js
+++ Coffee Machine/main.js
@@ -8,8 +8,21 @@ const coffeeCup = {
     coffee: 15
 }
 
-let cups = input("Write how many cups of coffee you will need:\n");
-console.log(`For ${cups} cups of coffee you will need:
-${coffeeCup.water * cups} ml of water
-${coffeeCup.milk * cups} ml of milk
-${coffeeCup.coffee * cups} g of coffee beans`);
+let amountWater = input("Write how many ml of water the coffee machine has:\n");
+let amountMilk = input("Write how many ml of milk the coffee machine has:\n");
+let amountCoffee = input("Write how many grams of coffee beans the coffee machine has:\n");
+let cups = Number(input("Write how many cups of coffee you will need:\n"));
+let calculateCups = [
+    Math.floor(amountWater / coffeeCup.water),
+    Math.floor(amountMilk / coffeeCup.milk),
+    Math.floor(amountCoffee / coffeeCup.coffee)
+];
+calculateCups.sort(function(a,b){return a - b});
+if (calculateCups[0] < cups) {
+    console.log(`No, I can make only ${calculateCups[0]} cups of coffee`);
+} else if (calculateCups[0] === cups) {
+    console.log("Yes, I can make that amount of coffee")
+} else {
+    let extra = calculateCups[0] - cups;
+    console.log(`Yes, I can make that amount of coffee (and even ${extra} more than that)`);
+}
