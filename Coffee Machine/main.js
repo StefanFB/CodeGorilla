3494011843e58b23d87cf0a993e38056a45bfb2b
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-06 17:17:37 +0100

    Stage 5 finished

--- Coffee Machine/main.js
+++ Coffee Machine/main.js
@@ -1,6 +1,48 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
+// Main part of program
+// Ask user what he wants to do
+function main() {
+    let task = input("Write action (buy, fill, take, remaining, exit):\n");
+
+// Buy-route: ask what user wants, then run that through the makeCoffee function
+    if (task === "buy") {
+        let order = input("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
+        switch (order) {
+            case "back":
+                main();
+                break;
+            case "1":
+            case "espresso":
+                makeCoffee(espresso);
+                break;
+            case "2":
+            case "latte":
+                makeCoffee(latte);
+                break;
+            case "3":
+            case "cappuccino":
+                makeCoffee(cappuccino);
+                break;
+            default:
+                console.log("Unrecognized coffee type\n");
+                main();
+        }
+    } else if (task === "fill") {
+        fillMachine();
+    } else if (task === "take") {
+        emptyMachineMoney();
+    } else if (task === "remaining") {
+        machineStatus();
+    } else if (task === "exit") {
+        return 0;
+    } else {
+        console.log("Unrecognized command\n")
+        main();
+    }
+}
+
 // Starting conditions of the coffee machine
 const coffeeMachine = {
     water: 400,
@@ -12,15 +54,16 @@ const coffeeMachine = {
 
 // Function to show the current status of the coffee machine
 function machineStatus() {
-    console.log(`The coffee machine has:
+    console.log(`\nThe coffee machine has:
 ${coffeeMachine.water} ml of water
 ${coffeeMachine.milk} ml of milk
 ${coffeeMachine.coffee} g of coffee beans
 ${coffeeMachine.cups} disposable cups
 $${coffeeMachine.money} of money\n`)
+    main();
 }
 
-// Declare several different coffees to sell with this machine using function constructor
+// Declare function constructor to easily add new coffee types
 function Coffee(drink, water, milk, coffee, money) {
     this.drink = drink;
     this.water = water;
@@ -37,12 +80,30 @@ const cappuccino = new Coffee("cappuccino", 200, 100, 12, 6);
 // Function to update the machine status after making a certain type of coffee
 // Subtract the used ingredients from the machine supply, add the cost of a drink to the machine's money
 function makeCoffee(coffee) {
-    coffeeMachine.water -= coffee.water;
-    coffeeMachine.milk -= coffee.milk;
-    coffeeMachine.coffee -= coffee.coffee;
-    coffeeMachine.cups--;
-    coffeeMachine.money += coffee.money;
-    machineStatus();
+    // First, check if all necessary ingredients are available
+    if (coffeeMachine.water < coffee.water) {
+        console.log("Sorry, not enough water\n");
+        main();
+    } else if (coffeeMachine.milk < coffee.milk) {
+        console.log("Sorry, not enough milk\n");
+        main();
+    } else if (coffeeMachine.coffee < coffee.coffee) {
+        console.log("Sorry, not enough coffee\n");
+        main();
+    } else if (coffeeMachine.cups <= 0) {
+        console.log("Sorry, not enough cups\n");
+        main();
+    }
+    // If all ingredients are available, proceed with making coffee, subtracting ingredients and adding money
+    else {
+        coffeeMachine.water -= coffee.water;
+        coffeeMachine.milk -= coffee.milk;
+        coffeeMachine.coffee -= coffee.coffee;
+        coffeeMachine.cups--;
+        coffeeMachine.money += coffee.money;
+        console.log("I have enough resources, making you a coffee!\n")
+        main();
+    }
 }
 
 // Function to fill the machine with new ingredients and cups
@@ -58,50 +119,14 @@ function fillMachine() {
     coffeeMachine.milk += milk;
     coffeeMachine.coffee += coffee;
     coffeeMachine.cups += cups;
-
-    // Display new, updated machine status
-    console.log("");
-    machineStatus();
+    main();
 }
 
 // Function to empty the money drawer from the machine
 function emptyMachineMoney() {
-    console.log(`I gave you $${coffeeMachine.money}\n`);
+    console.log(`\nI gave you $${coffeeMachine.money}\n`);
     coffeeMachine.money = 0;
-    machineStatus();
+    main();
 }
 
-// Main part of program
-// First display current status of the coffee machine
-machineStatus();
-
-// Ask user what he wants to do
-let task = input("Write action (buy, fill, take):\n");
-
-// Buy-route: ask what user wants, then run that through the makeCoffee function
-if (task === "buy") {
-    let order = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
-    switch (order) {
-        case "1":
-        case "espresso":
-            makeCoffee(espresso);
-            break;
-        case "2":
-        case "latte":
-            makeCoffee(latte);
-            break;
-        case "3":
-        case "cappuccino":
-            makeCoffee(cappuccino);
-            break;
-        default:
-            console.log("Unrecognized coffee type")
-    }
-// Fill-route
-} else if (task === "fill") {
-    fillMachine();
-} else if (task === "take") {
-    emptyMachineMoney();
-} else {
-    console.log("Unrecognized command")
-}
+main();
