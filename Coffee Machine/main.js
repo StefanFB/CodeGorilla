2939994bb285fa61f2eb9da20a05c3bca6c819ee
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-06 11:22:52 +0100

    Stage 1 finished

---
+++ Coffee Machine/main.js
@@ -0,0 +1,10 @@
+// You will need this in the following stages
+const input = require('sync-input')
+
+console.log("Starting to make a coffee");
+console.log("Grinding coffee beans");
+console.log("Boiling water");
+console.log("Mixing boiled water with crushed coffee beans");
+console.log("Pouring coffee into the cup");
+console.log("Pouring some milk into the cup");
+console.log("Coffee is ready!");
