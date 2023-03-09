ac5bce92e62da6462326446297c937ca1cea8923
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-09 12:54:33 +0100

    Stage 3

--- Simple Chatty Bot (Java)/SimpleBot.java
+++ Simple Chatty Bot (Java)/SimpleBot.java
@@ -4,14 +4,23 @@ import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        String name = scanner.nextLine();
+        String name = scan.nextLine();
 
-        System.out.printf("What a great name you have, %s!%n", name);
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scan.nextInt();
+        int remainder5 = scan.nextInt();
+        int remainder7 = scan.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
     }
 }
