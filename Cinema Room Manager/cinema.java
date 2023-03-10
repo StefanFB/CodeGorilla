3307d16290824d4ce30cdb95271d805a06ee6777
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-10 09:28:13 +0100

    Stage 1

---
+++ Cinema Room Manager/cinema.java
@@ -0,0 +1,13 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        int size = 8;
+
+        System.out.println("Cinema:\n  1 2 3 4 5 6 7 8");
+        for (int i = 1; i < size; i++) {
+            System.out.printf("%d S S S S S S S S\n", i);
+        }
+    }
+}
