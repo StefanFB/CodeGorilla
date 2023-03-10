a5e6e7d6f41d8d74c9bc372f3b2cfc7f9fc33fc3
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-10 16:39:36 +0100

    Stage 3

--- Cinema Room Manager/cinema.java
+++ Cinema Room Manager/cinema.java
@@ -12,16 +12,77 @@ public class Cinema {
         System.out.print("Enter the number of seats in each row:\n> ");
         int seats = scan.nextInt();
 
+        // Create full array within one print-for-loop
+        System.out.println("\nCinema:");
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= seats; j++) {
+                // Top-left corner should be empty
+                if (i == 0 && j == 0) {
+                    System.out.print("  ");
+                }
+                // First row should be the seatnumbers
+                else if (i == 0) {
+                    System.out.printf("%d ", j);
+                }
+                // First column should be the row number
+                else if (j == 0) {
+                    System.out.printf("%d ", i);
+                }
+                // All other coordinates should be S for seat
+                else {
+                    System.out.print("S ");
+                }
+            }
+            System.out.print("\n");
+        }
+
+        // Ask for a specific seat
+        System.out.print("\nEnter a row number:\n> ");
+        int selectedRow = scan.nextInt();
+        System.out.print("Enter a seat number in that row:\n> ");
+        int selectedSeat = scan.nextInt();
+
+        // Calculate ticket price for that seat
+        int price = 0;
         if (rows * seats < 60) {
-            int profit = rows * seats * 10;
-            System.out.println("Total income:\n$" + profit);
-            return;
+            price = 10;
         } else {
-            int front = rows / 2;
-            int back = rows - front;
-            int profit = front * seats * 10 + back * seats * 8;
-            System.out.println("Total income:\n$" + profit);
-            return;
+            if (selectedRow <= rows / 2) {
+                price = 10;
+            } else {
+                price = 8;
+            }
+        }
+
+        // Display ticket price
+        System.out.printf("\nTicket price: $%d\n\n", price);
+
+        // Display new seating arrangement, displaying chosen seat as B
+        System.out.println("\nCinema:");
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= seats; j++) {
+                // Top-left corner should be empty
+                if (i == 0 && j == 0) {
+                    System.out.print("  ");
+                }
+                // First row should be the seatnumbers
+                else if (i == 0) {
+                    System.out.printf("%d ", j);
+                }
+                // First column should be the row number
+                else if (j == 0) {
+                    System.out.printf("%d ", i);
+                }
+                // If coordinates match the chosen seat, print B
+                else if (i == selectedRow && j == selectedSeat){
+                    System.out.print("B ");
+                }
+                // All other coordinates should be S for seat
+                else {
+                    System.out.print("S ");
+                }
+            }
+            System.out.print("\n");
         }
     }
 }
