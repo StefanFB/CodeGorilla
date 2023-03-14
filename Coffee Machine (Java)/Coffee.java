4ebea6de096c620707b162ed3d1c0e127983e229
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-14 09:03:03 +0100

    Coffee-class for stage 2

---
+++ Coffee Machine (Java)/Coffee.java
@@ -0,0 +1,12 @@
+package machine;
+
+public class Coffee {
+    int water;
+    int milk;
+    int coffee;
+    Coffee(int water, int milk, int coffee){
+        this.water = water;
+        this.milk = milk;
+        this.coffee = coffee;
+    }
+}
