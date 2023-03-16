200124b24bc28e566aa1c13803df717f56a91ca4
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-16 09:48:20 +0100

    Stage 4

--- Coffee Machine (Java)/CoffeeMachine.java
+++ Coffee Machine (Java)/CoffeeMachine.java
@@ -4,37 +4,128 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String[] args) {
+
+    static void machineStatus(Machine coffeeMachine) {
+        System.out.printf("\nThe coffee machine has:\n" +
+                "%d ml of water\n" +
+                "%d ml of milk\n" +
+                "%d g of coffee beans\n" +
+                "%d disposable cups\n" +
+                "$%d of money\n",
+                coffeeMachine.water, coffeeMachine.milk, coffeeMachine.coffee, coffeeMachine.cups, coffeeMachine.money);
+    }
+
+    static void showMenu(Machine coffeemachine) {
         Scanner scan = new Scanner(System.in);
 
-        Coffee coffee = new Coffee(200, 50, 15);
-
-        // Read inputs: how much ingredients are in the machine, and how much coffee has to be made
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int availableWater = scan.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int availableMilk = scan.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int availableCoffee = scan.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cupsOfCoffee = scan.nextInt();
-
-        // Calculate how much ingredients are needed
-        int neededWater = cupsOfCoffee * coffee.water;
-        int neededMilk = cupsOfCoffee * coffee.milk;
-        int neededCoffee = cupsOfCoffee * coffee.coffee;
-
-        // Create sorted array with possible cups. Then compare index 0 with the demanded amount.
-        int[] possibleCups = {availableWater / coffee.water, availableMilk / coffee.milk, availableCoffee / coffee.coffee};
-        Arrays.sort(possibleCups);
-
-        if (cupsOfCoffee == possibleCups[0]) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (cupsOfCoffee < possibleCups[0]) {
-            int extra = possibleCups[0] - cupsOfCoffee;
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", extra);
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee", possibleCups[0]);
+        System.out.println("\nPlease state your action (buy, fill, take):");
+        String action = scan.nextLine();
+        switch (action) {
+            case "buy" -> buyCoffee(coffeemachine);
+            case "fill" -> fillMachine(coffeemachine);
+            case "take" -> takeMoney(coffeemachine);
+            default -> System.out.println("unknown command, exiting program");
         }
+    }
+
+    static void buyCoffee(Machine coffeemachine) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What do you want to buy? (1 - espresso, 2 - latte, 3 - cappuccino");
+        int choice = scan.nextInt();
+        switch (choice) {
+            case 1 -> {
+                makeCoffee(coffeemachine, "espresso");
+            }
+            case 2 -> {
+                makeCoffee(coffeemachine, "latte");
+            }
+            case 3 -> {
+                makeCoffee(coffeemachine, "cappuccino");
+            }
+            default -> {
+                System.out.println("Unknown coffee-type, exiting...");
+            }
+        }
+    }
+
+    static void fillMachine(Machine coffeemachine) {
+        Scanner scan = new Scanner(System.in);
+
+        // Ask user what they want to add to the coffee machine
+        System.out.println("Write how many ml of water you want to add:");
+        int addWater = scan.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        int addMilk = scan.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        int addCoffee = scan.nextInt();
+        System.out.println("Write how many disposable cups you want to add:");
+        int addCups = scan.nextInt();
+
+        // Add it to the coffee machine, then print the new status
+        coffeemachine.water += addWater;
+        coffeemachine.milk += addMilk;
+        coffeemachine.coffee += addCoffee;
+        coffeemachine.cups += addCups;
+        machineStatus(coffeemachine);
+    }
+
+    static void makeCoffee(Machine coffeemachine, String coffeeType) {
+
+        // Create new coffee-types
+        Coffee espresso = new Coffee("espresso", 250, 0, 16, 4);
+        Coffee latte = new Coffee("latte", 350, 75, 20, 7);
+        Coffee cappuccino = new Coffee("cappuccino", 200, 100, 12, 6);
+
+        // Choose correct coffee, subtract the necessary ingredients, show new machine status
+        switch (coffeeType) {
+            case "espresso" -> {
+                coffeemachine.water -= espresso.water;
+                coffeemachine.milk -= espresso.milk;
+                coffeemachine.coffee -= espresso.coffee;
+                coffeemachine.cups--;
+                coffeemachine.money += espresso.price;
+            }
+            case "latte" -> {
+                coffeemachine.water -= latte.water;
+                coffeemachine.milk -= latte.milk;
+                coffeemachine.coffee -= latte.coffee;
+                coffeemachine.cups--;
+                coffeemachine.money += latte.price;
+            }
+            case "cappuccino" -> {
+                coffeemachine.water -= cappuccino.water;
+                coffeemachine.milk -= cappuccino.milk;
+                coffeemachine.coffee -= cappuccino.coffee;
+                coffeemachine.cups--;
+                coffeemachine.money += cappuccino.price;
+            }
+            default -> {
+                System.out.println("Unknown coffee-type, exiting...");
+            }
+        }
+        machineStatus(coffeemachine);
+    }
+
+    static void takeMoney(Machine coffeemachine) {
+        Scanner scan = new Scanner(System.in);
+
+        // Inform user about how much money was taken from the machine
+        System.out.printf("I gave you $%d\n", coffeemachine.money);
+
+        // Update and show new status of machine
+        coffeemachine.money = 0;
+        machineStatus(coffeemachine);
+    }
+
+    public static void main(String[] args) {
+        // Create our machine with pre-defined amounts of water, milk, coffee, etc.
+        Machine coffeeMachine = new Machine();
+
+        // Print coffee machine's state
+        machineStatus(coffeeMachine);
+
+        // Show menu
+        showMenu(coffeeMachine);
     }
 }
