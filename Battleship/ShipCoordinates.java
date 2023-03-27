e6df0fc40bb9bb44ef439f19b9a1938efe31dd63
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-27 11:26:31 +0200

    Stage 2

---
+++ Battleship/ShipCoordinates.java
@@ -0,0 +1,15 @@
+package battleship;
+
+public class ShipCoordinates {
+    int y1;
+    int x1;
+    int y2;
+    int x2;
+
+    public ShipCoordinates(int y1, int x1, int y2, int x2) {
+        this.y1 = y1;
+        this.x1 = x1;
+        this.y2 = y2;
+        this.x2 = x2;
+    }
+}
