81f555c932382ac7de07e7db13ebeb1dbcea1d0f
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-04-20 13:05:13 +0200

    Stage 2

--- Tic-Tac-Toe with AI (Java)/Main.java
+++ Tic-Tac-Toe with AI (Java)/Main.java
@@ -8,27 +8,41 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         TicTacToe game = new TicTacToe();
+        AI ai = new EasyAI();
 
-        // Ask for initial setup of the 3x3 table
-        System.out.print("Enter the cells: ");
-        String[] setup = scan.nextLine().split("");
-
-        // Show board with the given setup
-        game.readSetup(setup);
+        // Setup and show board
+        game.boardSetup();
         game.showBoard();
 
-        // Get coordinates, keep asking until correct input
-        int inputCheck = -1;
-        while (inputCheck != 0) {
-            System.out.print("Enter the coordinates: ");
-            String[] input = scan.nextLine().split(" ");
-            inputCheck = game.checkInput(input);
+        // While the game is playing, alternate between user moves and AI moves
+        while (game.state != GameState.DRAW && game.state != GameState.X_WINS && game.state != GameState.O_WINS) {
+
+            // Depending on the whose turn it is, make human or AI move
+            if (game.state == GameState.PLAYER1) {
+
+                // Get human player move: get coordinates, keep asking until correct input
+                int inputCheck = -1;
+                while (inputCheck != 0) {
+                    System.out.print("Enter the coordinates: ");
+                    String[] input = scan.nextLine().split(" ");
+                    inputCheck = game.checkInput(input);
+                }
+            } else {
+
+                // Let AI make a move
+                String str = String.format("Making move level \"%s\"", ai.getName());
+                System.out.println(str);
+                int inputCheckAI = -1;
+                while (inputCheckAI != 0) {
+                    int[] MoveAI = ai.makeMove();
+                    inputCheckAI = game.checkInput(MoveAI);
+                }
+            }
+
+            // Show updated game board and check new game state
+            game.showBoard();
+            game.updateGameState();
+
         }
-
-        // Show updated game board
-        game.showBoard();
-
-        // Check for winning conditions, draw or not finished
-        game.updateGameState();
     }
 }
