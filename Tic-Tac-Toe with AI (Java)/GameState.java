e54402d91c166fc9fb3004fcd7fcada6121f89dc
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-04-28 10:42:27 +0200

    Stage 5 (project done)

--- Tic-Tac-Toe with AI (Java)/GameState.java
+++ Tic-Tac-Toe with AI (Java)/GameState.java
@@ -1,9 +1,9 @@
 package tictactoe;
 
 public enum GameState {
-    SETUP, PLAYER1, PLAYER2, DRAW, X_WINS, O_WINS, END
+    SETUP, PLAYER1, PLAYER2, DRAW, X_WINS, O_WINS, QUIT
 }
 
-enum player {
-    HUMAN, EASY_AI, MEDIUM_AI
+enum Player {
+    HUMAN, EASY_AI, MEDIUM_AI, HARD_AI
 }
