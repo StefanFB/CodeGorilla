c2a9b3186a44e6cf94e3724e845c5b7c8d7dc1b1
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-04-28 10:27:00 +0200

    Stage 5 (project done)

--- Tic-Tac-Toe with AI (Java)/Main.java
+++ Tic-Tac-Toe with AI (Java)/Main.java
@@ -9,48 +9,49 @@ public class Main {
 
         TicTacToe game = new TicTacToe();
 
-        while (game.state != GameState.END) {
+        while (game.state != GameState.QUIT) {
             // Ask user for player setup: PvP, PvAI, AIvAI. Check for correct command usage.
             int commandCheck = -1;
-            String[] startCommand = new String[5];
+            String[] inputCommand = new String[5];
             while (commandCheck != 0) {
-                System.out.print("Input command (\"start <player1> <player2>\" or \"exit\"): ");
-                startCommand = scan.nextLine().split(" ");
-                commandCheck = game.checkCommandUsage(startCommand);
+                // System.out.print("Input command (\"start <player1> <player2>\" or \"exit\"): ");
+                System.out.print("Input command: ");
+                inputCommand = scan.nextLine().split(" ");
+                commandCheck = game.checkCommands(inputCommand);
             }
 
             // Stop the game when the GameState is set to END via the exit-command
-            if (startCommand[0].equals("exit")) {
-                game.state = GameState.END;
+            if (inputCommand[0].equals("exit")) {
+                game.state = GameState.QUIT;
                 break;
             }
 
-            System.out.println("Player1: " + startCommand[1]);
-            System.out.println("Player2: " + startCommand[2]);
+            // System.out.println("Player1: " + inputCommand[1]);
+            // System.out.println("Player2: " + inputCommand[2]);
 
             // Create player1 as human or AI player
-            switch (startCommand[1]) {
-                case "user" -> game.playerX = player.HUMAN;
-                case "easy" -> game.playerX = player.EASY_AI;
-                case "medium" -> game.playerX = player.MEDIUM_AI;
-                case "hard" -> game.playerX = player.HARD_AI;
+            switch (inputCommand[1]) {
+                case "user" -> game.playerX = Player.HUMAN;
+                case "easy" -> game.playerX = Player.EASY_AI;
+                case "medium" -> game.playerX = Player.MEDIUM_AI;
+                case "hard" -> game.playerX = Player.HARD_AI;
             }
 
             // Create player2 as human or AI player
-            switch (startCommand[2]) {
-                case "user" -> game.playerO = player.HUMAN;
-                case "easy" -> game.playerO = player.EASY_AI;
-                case "medium" -> game.playerO = player.MEDIUM_AI;
-                case "hard" -> game.playerO = player.HARD_AI;
+            switch (inputCommand[2]) {
+                case "user" -> game.playerO = Player.HUMAN;
+                case "easy" -> game.playerO = Player.EASY_AI;
+                case "medium" -> game.playerO = Player.MEDIUM_AI;
+                case "hard" -> game.playerO = Player.HARD_AI;
             }
 
             // Create necessary instances of each player-type
-            PlayerType human = new HumanPlayerType();
+            PlayerType human = new HumanPlayer();
             PlayerType easyAI = new EasyAI();
             PlayerType mediumAI = new MediumAI();
-            // Player hardAI = new HardAI();
+            PlayerType hardAI = new HardAI();
 
-            // Setup and show board
+            // Show board
             game.boardSetup();
             game.showBoard();
 
@@ -58,7 +59,7 @@ public class Main {
             while (game.state != GameState.DRAW &&
                     game.state != GameState.X_WINS &&
                     game.state != GameState.O_WINS &&
-                    game.state != GameState.END) {
+                    game.state != GameState.QUIT) {
 
                 // Depending on the whose turn it is, make human or AI move
                 if (game.state == GameState.PLAYER1) {
@@ -66,19 +67,20 @@ public class Main {
                         case HUMAN -> human.makeMove(game);
                         case EASY_AI -> easyAI.makeMove(game);
                         case MEDIUM_AI -> mediumAI.makeMove(game);
+                        case HARD_AI -> hardAI.makeMove(game);
                     }
                 } else {
                     switch (game.playerO) {
                         case HUMAN -> human.makeMove(game);
                         case EASY_AI -> easyAI.makeMove(game);
                         case MEDIUM_AI -> mediumAI.makeMove(game);
+                        case HARD_AI -> hardAI.makeMove(game);
                     }
                 }
 
                 // Show updated game board and check new game state
                 game.showBoard();
                 game.updateGameState();
-
             }
         }
     }
