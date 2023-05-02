8c5c39cabf9bdb2925cc9b3292e3d3564f6b7a9b
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-02 11:56:04 +0200

    Stage 1

---
+++ Cinema Room REST Service (Java)/Room.java
@@ -0,0 +1,29 @@
+package cinema;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+public class Room {
+    @JsonProperty("total_rows")
+    public int rows;
+    @JsonProperty("total_columns")
+    public int columns;
+    @JsonProperty("available_seats")
+    public ArrayList<Seat> availableSeats = new ArrayList<>();
+
+    public Room(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.availableSeats = getAvailableSeats();
+    }
+
+    private ArrayList<Seat> getAvailableSeats() {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                availableSeats.add(new Seat(i,j));
+            }
+        }
+        return availableSeats;
+    }
+}
