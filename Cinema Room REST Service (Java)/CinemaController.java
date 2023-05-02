0815c4e1e12298a622676f2b2c0a6fbd9dcb865e
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-02 19:35:50 +0200

    Stage 4 (project done)

--- Cinema Room REST Service (Java)/CinemaController.java
+++ Cinema Room REST Service (Java)/CinemaController.java
@@ -27,7 +27,7 @@ public class CinemaController {
 
         // Check for empty post-request
         if (seatLocation == null) {
-            return new ResponseEntity(Map.of("error", "The number of a row or a column is out of bounds!"),
+            return new ResponseEntity<>(Map.of("error", "The number of a row or a column is out of bounds!"),
                     HttpStatus.BAD_REQUEST);
         }
 
@@ -36,7 +36,7 @@ public class CinemaController {
             int test1 = seatLocation.row;
             int test2 = seatLocation.column;
         } catch (Exception e) {
-            return new ResponseEntity(Map.of("error", "The number of a row or a column is out of bounds!"),
+            return new ResponseEntity<>(Map.of("error", "The number of a row or a column is out of bounds!"),
                     HttpStatus.BAD_REQUEST);
         }
 
@@ -46,22 +46,93 @@ public class CinemaController {
 
         // Check for out of bounds seat
         if (row < 1 || row > cinemaRoom.rows || column < 1 || column > cinemaRoom.columns) {
-            return new ResponseEntity(Map.of("error", "The number of a row or a column is out of bounds!"),
+            return new ResponseEntity<>(Map.of("error", "The number of a row or a column is out of bounds!"),
                     HttpStatus.BAD_REQUEST);
         }
 
-        // Get the corresponding seat
+        // Get the corresponding seat. Check availability else set it to false
         Seat seat = cinemaRoom.getSeat(row, column);
         if (!seat.available) {
-            return new ResponseEntity(Map.of("error", "The ticket has been already purchased!"),
+            return new ResponseEntity<>(Map.of("error", "The ticket has been already purchased!"),
+                    HttpStatus.BAD_REQUEST);
+        } else {
+            seat.available = false;
+        }
+
+        // Generate ticket with this seat
+        Ticket ticket = new Ticket(seat);
+        cinemaRoom.soldTickets.add(ticket);
+
+        // Add the price of the seat to the income
+        cinemaRoom.income += seat.price;
+
+        for (Ticket T : cinemaRoom.soldTickets) {
+            System.out.println(Map.of(
+                    "token", T.token,
+                    "ticket", T.seat.row + " " + T.seat.column
+            ));
+        }
+
+        return new ResponseEntity<>(Map.of(
+                "token", ticket.token,
+                "ticket", ticket.seat),
+                HttpStatus.OK);
+    }
+
+    @PostMapping("/return")
+    public ResponseEntity<?> returnTickets(@RequestBody String postRequest) {
+
+        // Check for empty post-request
+        if (postRequest == null) {
+            return new ResponseEntity<>(Map.of("error", "Wrong token!"),
                     HttpStatus.BAD_REQUEST);
         }
-        seat.available = false;
 
-        return new ResponseEntity(Map.of(
-                "row", seat.row,
-                "column", seat.column,
-                "price", seat.price),
-                HttpStatus.OK);
+        // System.out.println("Token is: " + postRequest);
+
+        // Parse the token into a correct token-string
+        String[] tokenSplitOne = postRequest.split(":");
+        String[] tokenSplitTwo = tokenSplitOne[1].split("\"");
+        String token = tokenSplitTwo[1];
+
+        // System.out.println("Token is: " + token);
+
+
+        // Check for invalid token, if token is set to "wrong" the token does not exist (via method)
+        Ticket ticket = cinemaRoom.getTicket(token);
+        if (ticket.token.equals("wrong")) {
+            return new ResponseEntity<>(Map.of("error", "Wrong token!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Else the token is valid, and we can return the ticket
+        // Also set the seat to be available again
+        else {
+            // Reset seat availability and deduct price from income
+            ticket.seat.available = true;
+            cinemaRoom.income -= ticket.seat.price;
+
+            return new ResponseEntity<>(Map.of(
+                    "returned_ticket", ticket.seat),
+                    HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/stats")
+    public ResponseEntity<?> showStats(@RequestParam(value="password", required = false) String password) {
+        if (password == null) {
+            return new ResponseEntity<>(Map.of("error", "The password is wrong!"),
+                    HttpStatus.UNAUTHORIZED);
+        } else if (password.equals("super_secret")) {
+            return new ResponseEntity<>(Map.of(
+                    "current_income", cinemaRoom.income,
+                    "number_of_available_seats", cinemaRoom.calcAvailableSeats(),
+                    "number_of_purchased_tickets", cinemaRoom.calcSoldTickets()
+                    ),
+                    HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(Map.of("error", "The password is wrong!"),
+                    HttpStatus.UNAUTHORIZED);
+        }
     }
 }
