d46ae7f85825a48bb52d2dd5248f9c8131f83473
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-02 16:12:57 +0200

    Stage 3

--- Cinema Room REST Service (Java)/Seat.java
+++ Cinema Room REST Service (Java)/Seat.java
@@ -1,18 +1,56 @@
 package cinema;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
 public class Seat {
 
-    @JsonProperty("row")
     int row;
-
-    @JsonProperty("column")
     int column;
+
+    @JsonProperty("price")
+    int price;
+
+    @JsonIgnore
+    boolean available;
 
     public Seat(int row, int column) {
         this.row = row;
         this.column = column;
+        this.price = setPrice();
+        this.available = true;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    private int setPrice() {
+        if (this.row <= 4) {
+            return 10;
+        } else {
+            return 8;
+        }
     }
 }
