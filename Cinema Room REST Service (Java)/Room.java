ed553f4386e46853d2b265773c3ea0d933d016de
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-02 19:35:13 +0200

    Stage 4 (project done)

--- Cinema Room REST Service (Java)/Room.java
+++ Cinema Room REST Service (Java)/Room.java
@@ -1,5 +1,6 @@
 package cinema;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
@@ -10,25 +11,55 @@ public class Room {
     @JsonProperty("total_columns")
     public int columns;
     @JsonProperty("available_seats")
-    public ArrayList<Seat> availableSeats = new ArrayList<>();
+    public ArrayList<Seat> allSeats = new ArrayList<>();
+
+    @JsonIgnore
+    public ArrayList<Ticket> soldTickets = new ArrayList<>();
+
+    @JsonIgnore
+    public int income;
 
     public Room(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
-        this.availableSeats = getAvailableSeats();
+        this.allSeats = setAllSeats();
+        this.income = 0;
     }
 
-    private ArrayList<Seat> getAvailableSeats() {
+    private ArrayList<Seat> setAllSeats() {
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= columns; j++) {
-                availableSeats.add(new Seat(i,j));
+                allSeats.add(new Seat(i,j));
             }
+        }
+        return allSeats;
+    }
+
+    public int calcAvailableSeats() {
+        int availableSeats = 0;
+        for (Seat seat : allSeats) {
+            if (seat.available) availableSeats++;
         }
         return availableSeats;
     }
 
+    public int calcSoldTickets() {
+        int totalSeats = rows * columns;
+        return totalSeats - calcAvailableSeats();
+    }
+
     public Seat getSeat(int row, int column) {
         int index = (row - 1) * columns + column -1;
-        return availableSeats.get(index);
+        return allSeats.get(index);
+    }
+
+    public Ticket getTicket(String uuidToken) {
+        for (Ticket T : soldTickets) {
+            if (T.token.equals(uuidToken)) {
+                System.out.printf("Ticket found with uuid %s!\n", uuidToken);
+                return T;
+            }
+        }
+        return new Ticket();
     }
 }
