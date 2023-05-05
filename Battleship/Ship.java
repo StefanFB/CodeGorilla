ebb49207927214a7539ef1834c8f5287697a61ff
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-05 16:03:03 +0200

    Stage 4

--- Battleship/Ship.java
+++ Battleship/Ship.java
@@ -3,10 +3,18 @@ package battleship;
 public class Ship {
     String name;
     int length;
+    int shipStartX;
+    int shipStartY;
+    boolean horizontal;
+    boolean destroyed;
 
     public Ship(String name, int length) {
         this.name = name;
         this.length = length;
+        this.shipStartX = 0;
+        this.shipStartY = 0;
+        this.horizontal = true;
+        this.destroyed = false;
     }
 
     static final public Ship carrier = new Ship("Aircraft Carrier", 5);
