fb8341a7aa517218cd95808c26b24c0b1d9285ed
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-05 16:07:29 +0200

    Stage 4

--- Battleship/GameState.java
+++ Battleship/GameState.java
@@ -1,9 +1,13 @@
 package battleship;
 
 enum GameState {
-    PREPARATION, READY
+    PREPARATION, START, PLAYING, END
 }
 
 enum Preparation {
     PLACE_SHIP_1, PLACE_SHIP_2, PLACE_SHIP_3, PLACE_SHIP_4, PLACE_SHIP_5, PREP_DONE
+}
+
+enum LastShot {
+    NONE, MISS, HIT, SUNK_SHIP
 }
