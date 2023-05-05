462bb921c2ff0880dc35c99450da93fbc7f12584
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-05 16:02:40 +0200

    Stage 4

--- Battleship/Main.java
+++ Battleship/Main.java
@@ -8,19 +8,24 @@ public class Main {
         game.showFullMap();
 
         // Place ships during preparation-phase
-        while (game.state != GameState.READY) {
+        while (game.state == GameState.PREPARATION) {
             game.handlePlacement();
             game.showFullMap();
             if (game.prepPhase == Preparation.PREP_DONE) {
-                game.state = GameState.READY;
+                game.state = GameState.START;
             }
         }
         // Inform user the game is about to start
         System.out.println("The game starts!");
+        System.out.printf("%s x = %d, y = %d, horizontal = %b",
+                Ship.battleship.name, Ship.battleship.shipStartX, Ship.battleship.shipStartY, Ship.battleship.horizontal);
 
-        // Let user shoot once
-        game.showMap();
-        game.handleFiring();
-        game.showFullMap();
+        // Let user shoot until all ships are sunk and the game ends
+        while (game.state != GameState.END) {
+            game.showMap();
+            game.handleFiring();
+            // Ships.showShipStatus();
+        }
+        System.out.print("You won. Congratulations!");
     }
 }
